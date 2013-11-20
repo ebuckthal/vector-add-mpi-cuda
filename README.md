5e8e1a0abdc499a04d206b8ad8571198b858366d
Eric Buckthal <ebuckthal@gmail.com>
2013-11-20 15:48:39 -0800

    final handin

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-vector-add-mpi-cuda
-===================
+1. make sure the Makefile points to the local installation of nvcc. (Already set up
+   for 302.
+
+2. run "make"
+
+3. run "mpirun -mca btl_tcp_if_include eth0 -H <list of hosts (302x15,302x14,...)> 
+   vector_add <binary_file_a> <binary_file_b>"
+
+4. output file will be hist.c
+
+5. diff as desired
+
+6. to run with text files instead of binary files, switch the comments on the 4
+   indicated lines in the vector_add.cpp file and make again
+
--- vector_add.cpp
+++ vector_add.cpp
@@ -18,6 +18,10 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+#define NUM_BINS 80
+#define HIST_MIN -20.0
+#define HIST_MAX 20.0
+
 #define MPI_CHECK(call) \
    if((call) != MPI_SUCCESS) { \
       cerr << "MPI error calling \""#call"\"\n"; \
@@ -49,31 +53,42 @@ int main(int argc, char *argv[])
    int *histogram_recv = NULL;
    int *histogram_final = NULL;
 
+   //UNCOMMENT THESE LINES FOR TEXT FILE READING
+   //int size_a = size_file(argv[1]);
+   //int size_b = size_file(argv[2]);
 
-
-   int size_a = size_file(argv[1]);
-   int size_b = size_file(argv[2]);
+   //UNCOMMENT THESE LINES FOR BINARY FILE READING
+   int size_a = size_file_binary(argv[1]);
+   int size_b = size_file_binary(argv[2]);
 
    if(commRank == 0) {
 
+      //root compares sizes of input files
       if(size_a != size_b) {
          cerr << "Input files must contain vectors of the same size" << endl;
          MPI_CHECK(MPI_Finalize());
          exit(-1);
       }
 
+      //weird results if we use too many machines on a small file
       if(size_a < commSize) {
          cerr << "There really isn't a need to parallelize such a small file across so many machines!" << endl;
          MPI_CHECK(MPI_Finalize());
          exit(-1);
       }
 
+      //size of root node includes the possible modulus in addition to its divided share
       size_node = size_a / commSize + (size_a % commSize);
+
+      //roots offset begins at one
       offset = 0;
 
    } else {
 
+      //node vector size
       size_node = size_a / commSize;
+
+      //nodes begin their offset starting at the end of the roots share
       offset = (size_a / commSize + (size_a % commSize)) + ((size_a / commSize) * (commRank-1));
    }
 
@@ -88,51 +103,77 @@ int main(int argc, char *argv[])
       exit(-1);
    }
 
-   //vector_file_binary(argv[1], vector_a, size_node, offset);
-   //vector_file_binary(argv[2], vector_b, size_node, offset);
-   vector_file(argv[1], vector_a, size_node, offset);
-   vector_file(argv[2], vector_b, size_node, offset);
+   //the next functions read the vector of size_node at offset into the specified float pointer
+
+   //COMMENT THESE LINES FOR BINARY FILE READING
+   vector_file_binary(argv[1], vector_a, size_node, offset);
+   vector_file_binary(argv[2], vector_b, size_node, offset);
+   
+   //UNCOMMENT THESE LINES FOR TEXT FILE READING
+   //vector_file(argv[1], vector_a, size_node, offset);
+   //vector_file(argv[2], vector_b, size_node, offset);
+
 
    if(NULL == (vector_bins = (int *)malloc(size_node * sizeof(int)))) {
       MPI_Finalize();
       exit(-1);
    }
 
+   //this CUDA function combines the vectors into a vector of the same size indicating which bin
+   //this added result should be counted towards
    computeAddGPU(vector_a, vector_b, vector_bins, size_node);
 
-   if(NULL == (histogram_node = (int *)malloc(80 * sizeof(int)))) {
+   if(NULL == (histogram_node = (int *)malloc(NUM_BINS * sizeof(int)))) {
       MPI_Finalize();
       exit(-1);
    }
-   memset(histogram_node, 0, 80 * sizeof(int));
+
+   //histogram_node represents each nodes histogram of the added and 'binned' initial vectors
+   memset(histogram_node, 0, NUM_BINS * sizeof(int));
    for(i = 0; i < size_node; i++) {
-      histogram_node[(vector_bins[i] > 79 ? 79 : vector_bins[i])]++;
+
+      //the 'binning' in the CUDA code will result in 0-NUM_BINS bins but we want 0-NUM_BINS where the
+      //largest numbers are in bin NUM_BINS-1
+      histogram_node[(vector_bins[i] > NUM_BINS-1 ? NUM_BINS-1 : vector_bins[i])]++;
    }
 
+
    if(commRank != 0) {
-      MPI_CHECK(MPI_Send(histogram_node, 80, MPI_INT, 0, 0, MPI_COMM_WORLD));
+      //if we are a node, send our histogram to the root
+      MPI_CHECK(MPI_Send(histogram_node, NUM_BINS, MPI_INT, 0, 0, MPI_COMM_WORLD));
+
    } else {
+      //if we are the root, accept everyones histogram and add it to our own
       for(j = 1; j < commSize; j++) {
 
-         if(NULL == (histogram_recv = (int *)malloc(80 * sizeof(int)))) {
+         if(NULL == (histogram_recv = (int *)malloc(NUM_BINS * sizeof(int)))) {
             MPI_Finalize();
             exit(-1);
          }
-         MPI_CHECK(MPI_Recv(histogram_recv, 80, MPI_INT, j, 0, MPI_COMM_WORLD, NULL));
+         MPI_CHECK(MPI_Recv(histogram_recv, NUM_BINS, MPI_INT, j, 0, MPI_COMM_WORLD, NULL));
 
-         for(i = 0; i < 80; i++) {
+         for(i = 0; i < NUM_BINS; i++) {
             histogram_node[i] += histogram_recv[i];
          }
 
          free(histogram_recv);
       }
 
+      //write the resultant histogram to a file
       FILE *fp = fopen("hist.c", "w");
-      for(j = 0; j < 80; j++) {
+      for(j = 0; j < NUM_BINS; j++) {
          fprintf(fp, "%d, %d\n", j, histogram_node[j]);
       }
       fclose(fp);
    }
+
+   free(vector_a);
+   free(vector_b);
+   free(vector_bins);
+   free(histogram_node);
+   free(histogram_gather);
+   free(histogram);
+   free(histogram_final);
 
    MPI_CHECK(MPI_Finalize());
    return 0;
